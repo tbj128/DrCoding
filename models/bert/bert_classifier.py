fbfd797858807268b7f420ae240a74d78faf7910
Tom Jin <tomjin@stanford.edu>
2020-03-12 15:44:20 -0700

    Rev

--- models/bert/bert_classifier.py
+++ models/bert/bert_classifier.py
@@ -128,7 +128,8 @@ class BertClassifierWithMetadataXS(BertPreTrainedModel):
     def __init__(self, config):
         super(BertClassifierWithMetadataXS, self).__init__(config)
         self.bert = BertModelWithMetadata(config)
-        self.preclassifier = nn.Linear(config.hidden_size, config.hidden_size)
+        self.preclassifier1 = nn.Linear(config.hidden_size, config.hidden_size)
+        self.preclassifier2 = nn.Linear(config.hidden_size, config.hidden_size)
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
         self.classifier = nn.Linear(config.hidden_size, config.num_labels)
         self.init_weights()
@@ -149,8 +150,9 @@ class BertClassifierWithMetadataXS(BertPreTrainedModel):
             metadata_ids=torch.repeat_interleave(metadata_ids, repeats=batch_increase_factor, dim=0)
         ) # bs * batch_increase_factor, metadata_len
         pooled_output = pooled_output.view(batch_size, batch_increase_factor, -1).mean(dim=1)
-        # pooled_output = self.preclassifier(pooled_output)
-        # pooled_output = nn.ReLU()(pooled_output)
+        pooled_output = self.preclassifier1(pooled_output)
+        pooled_output = self.preclassifier2(pooled_output)
+        pooled_output = nn.Tanh()(pooled_output)
         pooled_output = self.dropout(pooled_output)
         return self.classifier(pooled_output)
 
