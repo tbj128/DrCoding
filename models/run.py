3d0af3680939d0f388c8adc4c9ee69664237c212
Tom Jin <tomjin@stanford.edu>
2020-03-14 10:16:26 -0700

    Fix bug

--- models/run.py
+++ models/run.py
@@ -143,13 +143,15 @@ def predict_output(args, model, dev_data, device, batch_size=32, tokenizer=None)
                 input_mask = torch.tensor([f.input_mask for f in src_text], dtype=torch.long, device=device)
                 segment_ids = torch.tensor([f.segment_ids for f in src_text], dtype=torch.long, device=device)
                 tokens_metadata_text = tokenizer.tokenize(METADATA_FIXED + " " + METADATA_FIXED)
-                metadata_ids = tokenizer.convert_tokens_to_ids(tokens_metadata_text)
+                metadata_ids = torch.tensor(tokenizer.convert_tokens_to_ids(tokens_metadata_text), dtype=torch.long, device=device)
                 model_out = model(
                     input_ids,
                     segment_ids,
                     input_mask,
                     # metadata_input_ids=input_ids
-                    metadata_input_ids=metadata_ids
+
+                    metadata_input_ids=metadata_ids,
+                    meta_len=int(args['--meta-len'])
                 )
 
             elif args['--model'] == "reformer-metadata":
