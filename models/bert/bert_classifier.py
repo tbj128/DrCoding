a2780192d123683944826560345c93b0e10c8c5e
Tom Jin <tomjin@stanford.edu>
2020-03-14 10:42:08 -0700

    lkj

--- models/bert/bert_classifier.py
+++ models/bert/bert_classifier.py
@@ -174,8 +174,8 @@ class BertClassifierWithMetadataXS(BertPreTrainedModel):
         mask = (attn_mask.view(batch_size, batch_increase_factor, metadata_len).sum(dim=2) > 0).type(torch.float).unsqueeze(2)
         pooled_output = pooled_output.view(batch_size, batch_increase_factor, -1)
         pooled_output = pooled_output * mask
-        # pooled_output = torch.sum(pooled_output, dim=1) / torch.sum(mask.type(torch.float), dim=1)
-        pooled_output = torch.max(pooled_output, dim=1)[0]
+        pooled_output = torch.sum(pooled_output, dim=1) / torch.sum(mask.type(torch.float), dim=1)
+        # pooled_output = torch.max(pooled_output, dim=1)[0]
 
         pooled_output = self.dropout(pooled_output)
         return self.classifier(pooled_output)
